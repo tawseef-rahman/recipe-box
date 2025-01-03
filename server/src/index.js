6c1f19ab32dc22148f5d2420631df41ac67bd10f
Tawseef Rahman <tawseefrahman1@gmail.com>
2025-01-02 19:02:21 -0500

    update

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import { userRouter } from "./routes/users.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+mongoose.connect(
+  "mongodb+srv://tawseefrahman1:MERNpassword123@recipes.tmufk.mongodb.net/recipes?retryWrites=true&w=majority&appName=recipes",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started."));
